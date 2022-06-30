66bbff4b8fbba5fb6a9100ba1b3c5e88bc45c2e0
pereira299 <lucasper057@gmail.com>
2022-06-30 23:57:16 +0000

    up

--- src/pages/api/home.ts
+++ src/pages/api/home.ts
@@ -1,7 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import {parse} from "../../utils/csv"
 
 export default function home(req:NextApiRequest, res:NextApiResponse){
-    
+    parse()
     res.status(200).json({
         casos:{
             estado: [],
