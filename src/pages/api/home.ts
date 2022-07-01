da83e03940e989ca0ace2adc2e3f02d9b8bc6427
pereira299 <lucasper057@hotmail.com>
2022-06-30 21:43:37 -0300

    up

--- src/pages/api/home.ts
+++ src/pages/api/home.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import {parse} from "../../utils/csv"
 
 export default function home(req:NextApiRequest, res:NextApiResponse){
-    parse()
+    //parse(path.resolve(__dirname, 'assets', 'snake_case_users.csv')"../../files/COVIDBR_1.csv", {});
     res.status(200).json({
         casos:{
             estado: [],
