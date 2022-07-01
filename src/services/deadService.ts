598c87fa25fb0b7520c830d49f03d599a45ff7d3
pereira299 <lucasper057@hotmail.com>
2022-06-30 22:20:56 -0300

    up

--- src/services/deadService.ts
+++ src/services/deadService.ts
@@ -1,7 +1,29 @@
+import axios from "axios"
+type covid = {
+    cases: number,
+    datetime: Date,
+    deaths: number,
+    refuses: number,
+    state: string
+    suspects: number
+    uf: string
+    uid: number
+}
 export default function deadService() {
+    const data:any = axios.get("https://covid19-brazil-api.now.sh/api/report/v1")
     return {
-            estado: [],
-            idade: [],
-            data: []
+        casesByState: data.data.map((item:covid) => {
+            return {
+                state: item.state,
+                count: item.cases
+            }
+        }),
+        deathsByState: data.data.map((item:covid) => {
+            return {
+                state: item.state,
+                count: item.deaths
+            }
         }
+        )
+    }
 }
